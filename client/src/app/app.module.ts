1986a2aa8e9c9689c9848b2fb6bb47fa4beb4c23
jonathanj101 <jonatjimen@gmail.com>
2023-05-02 15:11:05 -0400

    imported libraries and components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+// Libraries
+
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+
+// Components
 
 import { AppComponent } from './app.component';
+import { UploadCsvComponent } from './views/upload-csv/upload-csv.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ToastComponent } from './views/toast/toast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadCsvComponent,
+    ToastComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatSnackBarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

33d01f0b82970057b22efe25c361948e24a0239b
jonathanj101 <jonatjimen@gmail.com>
2023-05-02 15:06:56 -0400

    created api services

---
+++ client/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { AppHttpService } from './app-http/app-http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private httpService: AppHttpService
+  ) { }
+
+  postExpensesCsv(file:any) {
+    return this.httpService.post_csv("csv/upload-csv",file);
+  }
+}

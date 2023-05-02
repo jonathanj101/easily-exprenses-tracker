69bb3c270c1bf4f31c4b83b16e5d5e37958dee8e
jonathanj101 <jonatjimen@gmail.com>
2023-05-01 22:23:44 -0400

    using upload_csv_file controller for now

--- server/routes/csv_upload_bp.py
+++ server/routes/csv_upload_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-from controllers.csv_upload_controller import upload_pdf_file
+from controllers.csv_upload_controller import upload_csv_file
 
 csv_upload_bp = Blueprint("csv_upload_bp", __name__)
 
-csv_upload_bp.route("/upload-csv", methods=["POST"])(upload_pdf_file)
+csv_upload_bp.route("/upload-csv", methods=["POST"])(upload_csv_file)
